e5aa388379a57cf411f2ba32e77e183bf6fb4757
ebrahimahmed97090 <ebrahimahmed97090@yahoo.com>
2019-12-13 11:49:19 +0200

    done

--- Js/app.js
+++ Js/app.js
@@ -2,14 +2,10 @@ const uparrow = document.querySelector(".uparrow");
 const dnarrow = document.querySelector(".dnarrow");
 const frmlist = document.querySelector('.lstaa');
 const lstg = document.querySelector('.lstg');
-let h = ''
+let h;
 const inpfield = document.getElementById("num");
-let delet = document.querySelectorAll(".del");
 let inputs = document.querySelectorAll(".npt");
-function hasClass(element, className) {
-    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
-}
-
+let g;
 uparrow.addEventListener('click', e => {
     e.preventDefault();
     if (inpfield.value < 0) {
@@ -45,27 +41,29 @@ lstg.addEventListener('click', e => {
     if (e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("delo")) {
         e.target.parentElement.parentElement.parentElement.remove();
     }
-    if (e.target.tagName === "P")
-    {
-       let c = e.target.textContent;
-        e.target.innerHTML = '<input class="npt" type="text" value="'+ c + '" name="list" id="">'
-       // e.target.parentElement.removeChild(e.target)
+    if (e.target.tagName === "P") {
+        let c = e.target.textContent;
+        e.target.innerHTML = '<input class="npt" type="text" value="' + c + '" name="list" id="">'
+        // e.target.parentElement.removeChild(e.target)
     }
     inputs = document.querySelectorAll(".npt");
 
-    inputs.forEach((input) =>{
-        input.addEventListener('change', c=>{
+    inputs.forEach((input) => {
+        input.addEventListener('change', c => {
+            g = input;
             h = input.value;
         })
     })
+    if ((g.parentElement.parentElement === e.target.parentElement.parentElement && e.target.tagName === "svg" && e.target.classList.contains("ado")) || (e.target.tagName === "span") && e.target.classList.contains("k") && g.parentElement.parentElement === e.target.parentElement) {
+        {
 
-    if ((e.target.tagName === "svg" && e.target.classList.contains("ado")) || (e.target.tagName === "span") && e.target.classList.contains("k")) {
-        e.target.parentElement.parentElement.firstChild.innerHTML= '<p>'+h+'</p>'
+            e.target.parentElement.parentElement.firstChild.innerHTML = '<p>' + h + '</p>'
+        }
     }
 
-    if (e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("ado")) {
-     
-        e.target.parentElement.parentElement.parentElement.firstChild.innerHTML= '<p>'+h+'</p>'
+    if (g.parentElement.parentElement === e.target.parentElement.parentElement.parentElement && e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("ado")) {
+
+        e.target.parentElement.parentElement.parentElement.firstChild.innerHTML = '<p>' + h + '</p>'
 
     }
 
