5da8039dc2f94738b87356a916792a3d245a6519
ebrahimahmed97090 <ebrahmahmed97090@gmail.com>
2020-01-01 20:11:40 +0200

    left if i wanted to edit list item changes another one and make p placeholder

--- Js/app.js
+++ Js/app.js
@@ -6,6 +6,8 @@ let h;
 const inpfield = document.getElementById("num");
 let inputs = document.querySelectorAll(".npt");
 let g;
+let body = document.querySelector('body');
+let inp = document.querySelectorAll('.pt');
 uparrow.addEventListener('click', e => {
     e.preventDefault();
     if (inpfield.value < 0) {
@@ -27,15 +29,17 @@ frmlist.addEventListener('submit', e => {
         let listitem = document.createElement("li");
         lstg.appendChild(listitem);
         listitem.classList.add('itm', 'list-group-item');
-        listitem.innerHTML = '<p>Click here to edit</p><span class="del"><i class="fas delo  fa-trash-alt"></i></span>' +
+        listitem.innerHTML = '<p class="npt">Click here to edit</p><span class="del"><i class="fas delo  fa-trash-alt"></i></span>' +
             '<span class="k"><i class="ado fas fa-check"></i></span>'
+        inputs = document.querySelectorAll(".npt");
+
+
     }
     inpfield.value = 0;
 
 });
 lstg.addEventListener('click', e => {
     if ((e.target.tagName === "svg" && e.target.classList.contains("delo")) || (e.target.tagName === "span") && e.target.classList.contains("del")) {
-
         e.target.parentElement.parentElement.remove();
     }
     if (e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("delo")) {
@@ -43,28 +47,49 @@ lstg.addEventListener('click', e => {
     }
     if (e.target.tagName === "P") {
         let c = e.target.textContent;
-        e.target.innerHTML = '<input class="npt" type="text" value="' + c + '" name="list" id="">'
-        // e.target.parentElement.removeChild(e.target)
-    }
-    inputs = document.querySelectorAll(".npt");
 
-    inputs.forEach((input) => {
-        input.addEventListener('change', c => {
-            g = input;
-            h = input.value;
+        e.target.innerHTML = '<input class="pt" autofocus type="text" value="' + c + '" name="list" id="">'
+        inp = document.querySelectorAll('.pt');
+        inputs = document.querySelectorAll(".npt");
+
+    }
+
+
+    lstg.addEventListener('click', c => {
+        if (c.target.tagName == "P")
+            h = c.target.innerHTML;
+        g = c.target;
+    });
+    lstg.addEventListener('mouseout', c => {
+        if (c.target.tagName == "INPUT")
+            h = c.target.value;
+        g = c.target;
+    });
+    lstg.addEventListener('change', c => {
+        if (c.target.tagName == "INPUT")
+            h = c.target.value;
+        g = c.target;
+    });
+
+    if ((g.parentElement.parentElement === e.target.parentElement.parentElement && e.target.tagName === "svg" && e.target.classList.contains("ado")) || (e.target.tagName === "span") && e.target.classList.contains("k") && g.parentElement.parentElement === e.target.parentElement) {
+        e.target.parentElement.parentElement.firstChild.innerHTML = h;
+    } else if (g.parentElement.parentElement === e.target.parentElement.parentElement.parentElement && e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("ado")) {
+        e.target.parentElement.parentElement.parentElement.firstChild.innerHTML = h;
+    } else if (e.target.tagName === "LI" && g.parentElement.parentElement == e.target && e.target.firstChild.firstChild.tagName === "INPUT") {
+        e.target.firstChild.innerHTML = h;
+    } else if (e.target.tagName === "LI" && e.target.firstChild.firstChild.tagName === "INPUT") {
+        e.target.parentElement.children.firstChild.innerHTML = e.target.parentElement.children.firstChild.firstChild.value;
+    }
+});
+body.addEventListener('click', ev => {
+    inputs = document.querySelectorAll(".npt");
+    inp = document.querySelectorAll('.pt');
+
+    inputs.forEach((put) => {
+        inp.forEach((inut) => {
+            if (ev.target != inut && !(ev.target.classList.contains("npt") && ev.target.firstCild.tagName == "INPUT")) {
+                inut.parentElement.innerHTML = inut.value;
+            }
         })
     })
-    if ((g.parentElement.parentElement === e.target.parentElement.parentElement && e.target.tagName === "svg" && e.target.classList.contains("ado")) || (e.target.tagName === "span") && e.target.classList.contains("k") && g.parentElement.parentElement === e.target.parentElement) {
-        {
-
-            e.target.parentElement.parentElement.firstChild.innerHTML = '<p>' + h + '</p>'
-        }
-    }
-
-    if (g.parentElement.parentElement === e.target.parentElement.parentElement.parentElement && e.target.tagName === "path" && e.target.getAttribute("fill") == "currentColor" && e.target.parentElement.classList.contains("ado")) {
-
-        e.target.parentElement.parentElement.parentElement.firstChild.innerHTML = '<p>' + h + '</p>'
-
-    }
-
 });
